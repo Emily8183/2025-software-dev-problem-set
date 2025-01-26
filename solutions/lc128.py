983f5dd58e5e853fc382afe01920dd796f673373
Emily <emily.zy.lin@gmail.com>
2025-01-26 16:18:02 -0600

    update format

--- solutions/lc128.py
+++ solutions/lc128.py
@@ -9,23 +9,23 @@ solution: hashset
 3, get the largest freq++
 
 '''
-    def longestConsecutive(self, nums: List[int]) -> int:
+def longestConsecutive(self, nums: List[int]) -> int:
 
-        numSet = set(nums) #不需要map,用set就可以. 只需要一步，
+    numSet = set(nums) #不需要map,用set就可以. 只需要一步，
 
-        max_freq = 0
+    max_freq = 0
 
-        for n in numSet:
-        #这里要加一步，check if the n is the smallest in the set
-            
-            if (n-1) not in numSet: 
-                freq = 0
+    for n in numSet:
+    #这里要加一步，check if the n is the smallest in the set
+        
+        if (n-1) not in numSet: 
+            freq = 0
 
-                while (n+freq) in numSet:
-                    freq += 1
-                max_freq = max(freq, max_freq)
+            while (n+freq) in numSet:
+                freq += 1
+            max_freq = max(freq, max_freq)
 
-        return max_freq
+    return max_freq
 
 
 
