18adb190e5acbd94183a27100ec9015ee3db81a0
Emily <emily.zy.lin@gmail.com>
2025-02-03 16:35:08 -0600

    update readme

---
+++ solutions/lc136-TBC.py
@@ -0,0 +1,9 @@
+'''
+condition: List[int] numbers
+goal: find the single number
+requirements: 1/ linear runtime: O(n); 2/ constant extra space: O(1)
+solution: 1/ hashset, but space is O(n), 2/ bit
+
+Code: TBC
+
+'''
